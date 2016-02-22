f0cfa89545143137602141ac8c6814650ff60dff
Adam Hammes <adam@hammes.io>
2016-02-22 16:30:53 -0500

    Create interaction diagrams

--- scripts/file_gen.py
+++ scripts/file_gen.py
@@ -13,16 +13,19 @@ def write_files(template_path, data_path):
 
     env = Environment(loader=FileSystemLoader('./'))
     template = env.get_template(template_path)
+    print(template)
 
     for author, cases in data.items():
         for case in cases:
+            print(make_title(case))
             with open(make_title(case), 'w') as f:
                 to_write = template.render(author=author, title=case)
+                print(to_write)
                 f.write(template.render(author=author, title=case))
 
 
 def main():
-    write_files('elaboration.jnj', 'hw_assignments.yaml')
+    write_files('interaction.jnj', 'hw_assignments.yaml')
 
 if __name__ == '__main__':
     main()
