d9dbeebdb4f4dd279787735a54b3772e5f35f1a8
Adam Hammes <adam@hammes.io>
2016-02-25 13:27:18 -0500

    Create Author class

---
+++ src/models/Author.java
@@ -0,0 +1,25 @@
+public class Author {
+    private String id;
+    private String name;
+
+    public Author(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+}
