08aae9ff98c4621f03a6528ce9cc3a316999c66a
Adam Hammes <adam@hammes.io>
2016-02-25 13:25:45 -0500

    Create Book class

---
+++ src/models/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    private String id;
+    private String title;
+
+    public Book(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle() {
+        this.title = title;
+    }
+}
