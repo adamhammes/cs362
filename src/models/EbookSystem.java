3a6ecbcb648397297fc037285706230c629ae543
Adam Hammes <adam@hammes.io>
2016-02-28 01:00:14 -0600

    Minor formatting tweaks

--- src/models/EbookSystem.java
+++ src/models/EbookSystem.java
@@ -1,23 +1,19 @@
 import java.sql.SQLException;
 
 public class EbookSystem {
-
-	
 	boolean addTag(String uid, String bookTitle, String tag) throws SQLException{
-		
 		DatabaseSupport db = new DatabaseSupport();
 		User user = db.getUser(uid);
 		
 		if (user != null){
 			boolean result = user.addTag(bookTitle, tag);
-					
-			if (result == false)
+			if (result == false) {
 				return false;
+			}
 			
 			System.out.println("results prior to database :" + result);
 			return db.putUser(user);
 		}
-		
 		return false;	
 	}
 }
