ec1839ea8afc4d560adb65a04f7a8af4a9206d92
Adam Hammes <adam@hammes.io>
2016-02-28 01:00:30 -0600

    Make addTag() public

--- src/models/EbookSystem.java
+++ src/models/EbookSystem.java
@@ -1,7 +1,7 @@
 import java.sql.SQLException;
 
 public class EbookSystem {
-	boolean addTag(String uid, String bookTitle, String tag) throws SQLException{
+	public boolean addTag(String uid, String bookTitle, String tag) throws SQLException{
 		DatabaseSupport db = new DatabaseSupport();
 		User user = db.getUser(uid);
 		
