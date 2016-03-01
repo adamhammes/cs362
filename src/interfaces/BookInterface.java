bc60220849f9769c5b3f88e49ad0bf5eb378f9f7
Adam Hammes <adam@hammes.io>
2016-03-01 17:55:58 -0600

    Extend Comparable<BookInterface>

--- src/interfaces/BookInterface.java
+++ src/interfaces/BookInterface.java
@@ -1,8 +1,12 @@
 package interfaces;
 
-public interface BookInterface {
-	
+import java.util.ArrayList;
+
+
+public interface BookInterface extends Comparable<BookInterface> {
+	public String getId();
 	public boolean addRating(int rating);
 	public boolean addTag(TagInterface t);
-
+	public ArrayList<TagInterface> getTags();
+	public Double averageRating();
 }
