d81ffaa21885c69c75c233c3063a371d4d33a146
Adam Hammes <adam@hammes.io>
2016-03-01 17:04:02 -0600

    Formatting

--- tests/TagTests.java
+++ tests/TagTests.java
@@ -4,20 +4,9 @@ import models.EbookSystem;
 
 public class TagTests {
 	public static void main(String[] args) throws SQLException{
-			
 		EbookSystem sys = new EbookSystem();
-		
 		boolean result = sys.addTag("nick", "Harry Potter and The Sorcerer's Stone", "My Book");
-	
+
 		System.out.println("result: " + result);
 	}
-	
-	
-//	public static void main(String[] args) throws SQLException{
-//		DatabaseSupport db = new DatabaseSupport();
-//		
-//		User user = new User("anna");
-//		db.putUser(user);
-//	}
-	
 }
