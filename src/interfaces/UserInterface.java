4974ff44c58fbe9d3c5135aa7a0f465a45a3dde1
Adam Hammes <adam@hammes.io>
2016-03-01 17:56:49 -0600

    Add getName() and getTags() method

--- src/interfaces/UserInterface.java
+++ src/interfaces/UserInterface.java
@@ -1,12 +1,13 @@
 package interfaces;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface UserInterface {
-	
+	public String getName();
 	public boolean addTag(String bookTitle, String tag);
 	public boolean addBook(String bid, String title);
 	public List<BookInterface> getAllBooks();
 	public boolean removeTag(String bookTitle, String tag);
-	
+	public ArrayList<TagInterface> getTags();
 }
