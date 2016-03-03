80ea9ba275634ecd272f6db576c05be17aedae20
Adam Hammes <adam@hammes.io>
2016-03-02 21:15:39 -0600

    Remove comments

--- tests/TagTests.java
+++ tests/TagTests.java
@@ -7,13 +7,6 @@ import models.Book;
 import models.User;
 
 public class TagTests {
-//	public static void main(String[] args) throws SQLException{
-//		EbookSystem sys = new EbookSystem();
-//		boolean result = sys.addTag("nick", "Harry Potter and The Sorcerer's Stone", "Im out of ideas");
-//
-//		System.out.println("result: " + result);
-//	}
-	
 	public static void main(String[] args) throws SQLException{
 		DatabaseSupport db = new DatabaseSupport();
 		User u = (User) db.getUser("nick");
@@ -26,19 +19,6 @@ public class TagTests {
 			}
 			System.out.println();
 		}
-		
-//		Tag t = ((Tag) u.userTags.get("awesome"));
-//		t.books.get(0).getTags().remove(t);
-//		u.userTags.remove("awesome");
-//		
-//		for (BookInterface b : u.userBooks.values()){
-//			System.out.println("book: " + ((Book) b).getTitle());
-//			System.out.print("\tTags: ");
-//			for (TagInterface at : b.getTags()){
-//				System.out.print(at.getName() + " ");
-//			}
-//			System.out.println();
-//		}
 		
 		db.putUser(u);
 	}
