8dfae375c763eccb34540137d6c4774c07f5ac6d
hammesa <hammesa@iastate.edu>
2016-03-07 13:36:44 -0500

    Delete print statements, add usage

--- scripts/file_gen.py
+++ scripts/file_gen.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 import yaml
+import sys
 
 
 def make_title(title):
@@ -13,20 +14,22 @@ def write_files(template_path, data_path):
 
     env = Environment(loader=FileSystemLoader('./'))
     template = env.get_template(template_path)
-    print(template)
 
     for author, cases in data.items():
         for case in cases:
-            print(make_title(case))
             with open(make_title(case), 'w') as f:
                 to_write = template.render(author=author, title=case)
-                print(to_write)
                 f.write(template.render(author=author, title=case))
 
 
 def main():
+    if (len(sys.argv) != 3 ):
+        print('Usage: python3 file_gen.py template.jnj data.yaml')
+        sys.exit(1)
+
     template_path = sys.argv[1]
-    write_files(template_path, 'hw_assignments.yaml')
+    data_path = sys.argv[2]
+    write_files(template_path, data_path)
 
 if __name__ == '__main__':
     main()
