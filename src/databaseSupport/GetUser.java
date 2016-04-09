2d76e3d4974c59cef2cc1144d562f9d5c72d34d2
Nicholas <njriesen@iastate.edu>
2016-04-08 22:00:26 -0500

    Split Get User into smaller methods

--- src/databaseSupport/GetUser.java
+++ src/databaseSupport/GetUser.java
@@ -18,31 +18,45 @@ class GetUser {
 		
 		User user = null;
 		
+		user = retreiveUser(conn, uid);
+		retreiveBooks(conn, user, uid);
+		retreiveTags(conn, user, uid);
+				
+		return user;
+	}
+	
+	
+	private static User retreiveUser(Connection conn, String uid) throws SQLException{
 		//Get User
 		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM account WHERE account_name = ?;");
 		stmt.setString(1, uid);
+		System.out.println(stmt);
 		ResultSet results = stmt.executeQuery();
 		
 		if (results.next()) {
-			user = new User(results.getString("account_name"));
+			return new User(results.getString("account_name"));
 		} else {
 			return null;
-		}
-		
-		//Get Books
-		stmt = conn.prepareStatement("SELECT book.book_id, book.title FROM account_book join book on account_book.book_id=book.book_id where account_book.account_name=?;");
+		}	
+	}
+	
+	
+	private static void retreiveBooks(Connection conn, User user, String uid) throws SQLException{
+			PreparedStatement stmt = conn.prepareStatement("SELECT book.book_id, book.title FROM account_book join book on account_book.book_id=book.book_id where account_book.account_name=?;");
+			stmt.setString(1, uid);
+			ResultSet results = stmt.executeQuery();
+			
+			while(results.next()){
+				Book toAdd = new Book(results.getString("book_id"), results.getString("title"));
+				user.userBooks.put(toAdd.getTitle(), toAdd);
+			}
+	}
+	
+	
+	private static void retreiveTags(Connection conn, User user, String uid) throws SQLException{
+		PreparedStatement stmt = conn.prepareStatement("SELECT Book_tag.book_id, tag, title FROM Book_tag inner join book on Book_tag.book_id=book.book_id WHERE account_name = ?;");
 		stmt.setString(1, uid);
-		results = stmt.executeQuery();
-		
-		while(results.next()){
-			Book toAdd = new Book(results.getString("book_id"), results.getString("title"));
-			user.userBooks.put(toAdd.getTitle(), toAdd);
-		}
-		
-		//Get Tags
-		stmt = conn.prepareStatement("SELECT Book_tag.book_id, tag, title FROM Book_tag inner join book on Book_tag.book_id=book.book_id WHERE account_name = ?;");
-		stmt.setString(1, uid);
-		results = stmt.executeQuery();
+		ResultSet results = stmt.executeQuery();
 		
 		while(results.next()){
 			TagInterface tag = user.userTags.get(results.getString("tag"));
@@ -54,8 +68,6 @@ class GetUser {
 			tag.addBook(book);
 			book.addTag(tag);
 		}
-		
-		return user;
 	}
 	
 }
