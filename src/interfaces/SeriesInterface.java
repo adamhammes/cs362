4dff100341ddb6767872ab7e1a83f679fadb27c4
Adam Hammes <adam@hammes.io>
2016-04-19 14:35:21 -0500

    Reformat

--- src/interfaces/SeriesInterface.java
+++ src/interfaces/SeriesInterface.java
@@ -3,17 +3,11 @@ package interfaces;
 import java.util.ArrayList;
 
 public interface SeriesInterface {
-
 	boolean setName(String name);
-
 	String getName();
-
 	String getId();
 
 	boolean addBook(BookInterface book);
-
 	ArrayList<BookInterface> getBooks();
-
 	boolean removeBook(String bid);
-
 }
