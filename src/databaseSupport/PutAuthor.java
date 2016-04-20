bd85227e6cdddb979edf91e59e519f13854dbf7e
Nicholas <njriesen@iastate.edu>
2016-04-20 12:26:39 -0500

    Db Put and get support Author description

--- src/databaseSupport/PutAuthor.java
+++ src/databaseSupport/PutAuthor.java
@@ -20,16 +20,19 @@ class PutAuthor extends Putable {
 		putAuthor(conn);
 		putBooks(conn);
 		removeOldBooks(conn);
-		return false;
+		return true;
 	}
 
 	
 	private void putAuthor(Connection conn) throws SQLException {
-		PreparedStatement stmt = conn.prepareStatement("INSERT INTO author VALUES (?, ?) "
-				+ "ON CONFLICT (author_id) DO UPDATE SET author_name = ?;");
+		PreparedStatement stmt = conn.prepareStatement("INSERT INTO author VALUES (?, ?, ?) "
+				+ "ON CONFLICT (author_id) DO UPDATE SET author_name = ?, description = ?;");
 		stmt.setString(1, author.getId());
 		stmt.setString(2, author.getName());
-		stmt.setString(3, author.getName());
+		stmt.setString(3, author.retreveDescription());
+		
+		stmt.setString(4, author.getName());
+		stmt.setString(5, author.retreveDescription());
 		stmt.executeUpdate();
 		alreadyStoredAuthors.add(author.getId());
 	}
@@ -47,7 +50,7 @@ class PutAuthor extends Putable {
 	private void removeOldBooks(Connection conn) throws SQLException {
 		StringBuffer rmSQL = new StringBuffer("DELETE FROM book_author WHERE (book_id, author_id) NOT IN ");
 		rmSQL.append("(SELECT * FROM book_author WHERE author_id != ?");
-		for (int i = 0; i < author.getBooks().size() - 1; i++)
+		for (int i = 0; i < author.getBooks().size(); i++)
 			rmSQL.append( "OR book_id = ? ");
 		rmSQL.append(");");
 		
