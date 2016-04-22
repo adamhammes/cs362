99b5f60e6dec95aad93ca94065461f6154daff51
nevets2707 <nevets27072@gmail.com>
2016-04-22 04:43:39 -0500

    Basic GUI implemented

---
+++ src/ui/GUI.java
@@ -0,0 +1,82 @@
+package ui;
+
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import oracle.jrockit.jfr.JFR;
+
+@SuppressWarnings("serial")
+public class GUI extends JPanel implements ActionListener {
+
+		JTextField text;
+		JTextArea area;
+		Controller controller;
+		
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String in = text.getText();
+			area.append(in + "\n");
+			text.selectAll();
+			
+			area.append(controller.command(in) + "\n");
+			
+			
+			area.setCaretPosition(area.getDocument().getLength());
+			
+		}
+		
+		public GUI(){
+		
+			super(new GridBagLayout());
+			
+			text = new JTextField(20);
+			text.addActionListener(this);
+			
+			area = new JTextArea(5, 20);
+			area.setEditable(false);
+			JScrollPane pane = new JScrollPane(area);
+			
+			GridBagConstraints c = new GridBagConstraints();
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			add(text, c);
+			
+			c.fill = GridBagConstraints.BOTH;
+			c.weightx = 1.0;
+			c.weighty = 1.0;
+			add(pane, c);
+			
+			controller = new Controller();
+			
+		}
+		
+		private static void createAndShowGUI(){
+			JFrame f = new JFrame("eBook Library");
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			f.add(new GUI());
+			
+			f.pack();
+			f.setVisible(true);
+		}
+		
+		
+		public static void main(String[] args){
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                createAndShowGUI();
+	            }
+	        });
+		}
+		
+		
+}
+	
+
---
+++ src/ui/iController.java
@@ -0,0 +1,45 @@
+package ui;
+
+import java.util.List;
+
+import interfaces.BookInterface;
+import interfaces.ReviewInterface;
+
+public interface iController {
+
+	
+	public String command(String command);
+	
+	// Adam Iteration 1
+	public String addUser(String input);
+	public String addBook(String input);
+	public String addVersion(String input);
+	
+	// Nick Iteration 1
+	public String addTag(String input);
+	public String removeTag(String input);
+	public String getBooksWithTag(String input);
+
+	// Steve Iteration 1
+	public String displayAllBooks(String input);
+	public String addRating(String input);
+	public String displayAllBooksByRating(String input);
+	
+	// Adam Iteration 2
+	public String searchBySeries(String input);
+	public String addBookToSeries(String input);
+	public String removeBookFromSeries(String input);
+	
+	
+	// Nick Iteration 2
+	public String changeRating(String input);
+	public String removeRating(String input);
+	public String getReviews(String input);
+	
+	// Steve Iteration 2
+	public String addDescription(String input);
+	public String editDescription(String input);
+	public String removeDescription(String input);
+	public String retrieveDescription(String input);
+	
+}
