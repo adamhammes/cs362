b5a535462a1e1ca6012bbb60e4cc2fe6ef7712d8
Adam Hammes <adam@hammes.io>
2016-04-25 14:27:59 -0500

    Unused import

--- tests/databaseTests/BookDbTests.java
+++ tests/databaseTests/BookDbTests.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.sun.xml.internal.ws.api.addressing.AddressingVersion;
-
 import databaseSupport.DatabaseSupport;
 import interfaces.AuthorInterface;
 import interfaces.BookInterface;
